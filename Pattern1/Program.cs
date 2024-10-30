e5363e1faf8291735a36d7ef00821a0ad9cd4e20
vismayrathore123 <vismayrathore2021@gmail.com>
2024-10-30 23:41:34 +0530

    first commit

---
+++ Pattern1/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+namespace Pattern1
+{
+class Program
+{
+        static void Main()
+        {
+            int rows = 5;
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int j = 1; j <= rows; j++)
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine();
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
