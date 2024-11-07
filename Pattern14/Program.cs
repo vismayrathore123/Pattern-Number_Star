84ce68b1f54619608316966c9c7c93d1ef8b6c09
vismayrathore123 <vismayrathore2021@gmail.com>
2024-11-07 11:35:57 +0530

    added pattern

---
+++ Pattern14/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for( int i = 1; i <= 5; i++)
+            {
+                for(int j=1;j<=5-i+1; j++)
+                {
+                    Console.Write("*");  
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+ 
+    }
+}
