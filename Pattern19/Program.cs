1f38e771d85673c93921f4f72e737a0a02566746
vismayrathore123 <vismayrathore2021@gmail.com>
2024-11-11 11:10:15 +0530

    Pattern19

---
+++ Pattern19/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Pattern19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i=7;i>=1; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    if (j % 2 == 1)
+                    {
+                        Console.Write("1");
+                    }
+                    else
+                    {
+                        Console.Write("0");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
