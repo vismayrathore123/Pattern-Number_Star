bf2a9bb3687d5858bc762123982b8073ffcfaed1
vismayrathore123 <vismayrathore2021@gmail.com>
2024-11-11 10:54:33 +0530

    pattern 18

---
+++ Pattern18/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Pattern18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 7; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    if (j % 2 == 1)
+                    {
+                        Console.Write("1");
+                    }
+                    else
+                    {
+                        Console.Write("0");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
