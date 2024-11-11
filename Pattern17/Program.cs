3f45a39622e3be6493f87d2809332d3c5b190192
vismayrathore123 <vismayrathore2021@gmail.com>
2024-11-11 10:40:28 +0530

    pattern17

---
+++ Pattern17/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Pattern17
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j < i ; j++)
+                {
+                    Console.Write(i);
+                }
+                for (int k = i ; k <= 5; k++)
+                {
+                    Console.Write(k);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
